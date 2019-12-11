8e9b76ae2443d7cb5d230254f1e29ad74c93e3ad
WendySegaar <1035706@mborijnland.nl>
2019-12-11 15:25:48 +0100

    Add actionbar to OverOnsActivity

--- app/src/main/java/com/example/amazigh_app2/OverOnsActivity.java
+++ app/src/main/java/com/example/amazigh_app2/OverOnsActivity.java
@@ -2,6 +2,8 @@ package com.example.amazigh_app2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 public class OverOnsActivity extends AppCompatActivity {
@@ -10,5 +12,10 @@ public class OverOnsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_over_ons);
+        getSupportActionBar().setTitle("Over ons");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setBackgroundDrawable(
+                new ColorDrawable(Color.parseColor("#FFEA2B")));
+
     }
 }
