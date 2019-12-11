88b29d5fc9aa5905dca4f6f759ff3c8e00382f6a
WendySegaar <1035706@mborijnland.nl>
2019-12-11 15:25:39 +0100

    Add actionbar to ScoresActivity

--- app/src/main/java/com/example/amazigh_app2/ScoresActivity.java
+++ app/src/main/java/com/example/amazigh_app2/ScoresActivity.java
@@ -2,6 +2,8 @@ package com.example.amazigh_app2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 public class ScoresActivity extends AppCompatActivity {
@@ -10,5 +12,9 @@ public class ScoresActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scores);
+        getSupportActionBar().setTitle("Scores");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setBackgroundDrawable(
+                new ColorDrawable(Color.parseColor("#D90D0D")));
     }
 }
