29aee983a0ff8d14b726b4463ca068f977ea2b3b
WendySegaar <1035706@mborijnland.nl>
2019-12-11 15:26:03 +0100

    Add actionbar to CategorieActivity

--- app/src/main/java/com/example/amazigh_app2/CategorieActivity.java
+++ app/src/main/java/com/example/amazigh_app2/CategorieActivity.java
@@ -2,6 +2,8 @@ package com.example.amazigh_app2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 public class CategorieActivity extends AppCompatActivity {
@@ -10,5 +12,9 @@ public class CategorieActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categorie);
+        getSupportActionBar().setTitle("Categorieën");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setBackgroundDrawable(
+                new ColorDrawable(Color.parseColor("#990099")));
     }
 }
