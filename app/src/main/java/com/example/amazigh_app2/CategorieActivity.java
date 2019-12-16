4655ab4285969b0b51a7e2654bbd0f517b34c59a
WendySegaar <1035706@mborijnland.nl>
2019-12-16 13:59:26 +0100

    Change images to categorie images

--- app/src/main/java/com/example/amazigh_app2/CategorieActivity.java
+++ app/src/main/java/com/example/amazigh_app2/CategorieActivity.java
@@ -2,6 +2,7 @@ package com.example.amazigh_app2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
@@ -18,22 +19,27 @@ public class CategorieActivity extends AppCompatActivity {
 
     int[] images = {
             R.drawable.dieren01_egel,
-            R.drawable.dieren01_ezel,
-            R.drawable.dieren01_geit,
-            R.drawable.dieren01_hond,
-            R.drawable.dieren01_jakhals,
-            R.drawable.dieren01_kat,
-            R.drawable.dieren01_kikker,
+            R.drawable.fruit_sinaasappel,
+            R.drawable.insecten_mier,
+            R.drawable.groente_doperwten,
+            R.drawable.dieren02_vos,
+            R.drawable.eten_koekjes,
+            R.drawable.kleding_broek,
+            R.drawable.weer_lente,
+            R.drawable.kleuren_geel,
+
     };
 
     String[] names = {
-            "Egel",
-            "Ezel",
-            "Geit",
-            "Hond",
-            "Jakhals",
-            "Kat",
-            "Kikker"
+            "Dieren 1",
+            "Fruit",
+            "Insecten",
+            "Groente",
+            "Dieren 2",
+            "Eten",
+            "Kleding",
+            "Weer",
+            "Kleuren",
     };
 
     @Override
@@ -49,6 +55,20 @@ public class CategorieActivity extends AppCompatActivity {
 
         CustomAdaptor customAdapter = new CustomAdaptor();
         listView.setAdapter(customAdapter);
+    }
+
+    public void redirect(String categorie) {
+        Bundle bundle = getIntent().getExtras();
+        if (bundle.getString("pagina").equals("quiz")) {
+            Intent intent = new Intent(this, QuizActivity.class);
+            intent.putExtra("categorie", categorie);
+            startActivity(intent);
+        } else {
+            Intent intent = new Intent(this, OefenenActivity.class);
+            intent.putExtra("categorie", categorie);
+            startActivity(intent);
+        }
+
     }
 
     class CustomAdaptor extends BaseAdapter {
@@ -69,8 +89,14 @@ public class CategorieActivity extends AppCompatActivity {
         }
 
         @Override
-        public View getView (int position, View convertView, ViewGroup parent) {
+        public View getView (final int position, View convertView, ViewGroup parent) {
             View view = getLayoutInflater().inflate(R.layout.listviewlayout, null);
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    redirect(names[position]);
+                }
+            });
             ImageView imageView = view.findViewById(R.id.imageView);
             TextView textView = view.findViewById(R.id.textView);
             imageView.setImageResource(images[position]);
